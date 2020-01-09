97eeb9f15db8cc21fa10599bae351cb7b3847dc1
Diana Wendt <dianakw@gmail.com>
2020-01-09 19:55:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,7 +20,7 @@ def fourth_challenge
       :heroine_friends => [
         {:name => "Steven",
         :age => "30",
-        :attitdue => "confused"},
+        :attitude => "confused"},
         {:name => "Nurse",
         :age => "44",
         :attitude => "worried"}
