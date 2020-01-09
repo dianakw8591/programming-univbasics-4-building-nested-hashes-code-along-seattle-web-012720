ceb976717bcd0be9add5d35e31139001464f91d7
Diana Wendt <dianakw@gmail.com>
2020-01-09 19:55:34 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,7 +10,7 @@ def fourth_challenge
         :attitude => "worried"},
         {:name => "Mercutio",
         :age => "18",
-        :attitdue => "hot-headed"}
+        :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
